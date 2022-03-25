1b5657df6f64f090abd3d4ab4e4212a838722168
xyhsy11 <xyhsy11@gmail.com>
2022-03-25 21:03:12 +0800

    Site updated: 2022-03-25 21:03:10

---
+++ Servlet/ServletDemo01.java
@@ -0,0 +1,33 @@
+package com.thomas.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class ServletDemo01 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init");
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+            System.out.println("Hello Servlet Demo1");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+
+    }
+}
